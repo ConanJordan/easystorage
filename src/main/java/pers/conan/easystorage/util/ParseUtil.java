8774d7b67f91b6e83cdda41c6efd5e01bb4b40e7
ConanJordan <1042583309@qq.com>
2020-04-06 19:28:15 +0800

    修改：编辑查询项

--- src/main/java/pers/conan/easystorage/util/ParseUtil.java
+++ src/main/java/pers/conan/easystorage/util/ParseUtil.java
@@ -1,7 +1,11 @@
 package pers.conan.easystorage.util;
 
 import java.util.Collection;
+import java.util.List;
+
+import pers.conan.easystorage.parse.Condition;
 import pers.conan.easystorage.parse.Module;
+import pers.conan.easystorage.parse.Sort;
 
 /**
  * 类：解析工具
@@ -27,20 +31,65 @@ public class ParseUtil {
 
     /**
      * 编辑查询项
-     * @param module
+     * @param selections
      * @param table
-     * @param commaPrefix
      * @return
      */
-    public static String editSelections(Module module, String table, boolean commaPrefix) {
-        StringBuilder selection = new StringBuilder();
+    public static String editSelections(List<Module> selections, String table) {
+        StringBuilder sb = new StringBuilder();
 
-        selection.append(commaPrefix ? ", " : " ");  // 逗号
-        selection.append(ParseUtil.isEmpty(table) ? "" : (table + "."));  // 表名
-        selection.append(module.getName());  // 字段名
-        selection.append(ParseUtil.isEmpty(module.getName()) ? "" : (" AS " + module.getAlias()));  // 别名
+        for (int i = 0; i < selections.size(); i ++) {
+            Module module = selections.get(i);
 
-        return selection.toString();
+            if (i == 0) {  // 没有逗号
+                sb.append(ParseUtil.isEmpty(table) ? "" : (table + "."));  // 表名
+                sb.append(module.getName());  // 字段名
+                sb.append(ParseUtil.isEmpty(module.getName()) ? "" : (" AS " + module.getAlias()));  // 别名
+            } else {
+                sb.append(" , " + (ParseUtil.isEmpty(table) ? "" : (table + ".")));  // 表名
+                sb.append(module.getName());  // 字段名
+                sb.append(ParseUtil.isEmpty(module.getName()) ? "" : (" AS " + module.getAlias()));  // 别名
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // 编辑查询条件
+    public static String editConditons(Condition condtion, boolean commaPrefix) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(commaPrefix ? ", " : " ");  // 逗号
+
+        // TODO
+        return null;
+
+    }
+
+    // 编辑排序条件
+    public static String editSort(Sort sort) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < sort.getSorts().size(); i ++) {
+
+            Module module = sort.getSorts().get(i);
+
+            if (i == 0) {  // 没有逗号
+                if (ParseUtil.isEmpty(module.getAlias())) {  // 有别名，别名优先
+                    sb.append(module.getAlias() + (module.isAsc() ? " ASC " : " DESC "));
+                } else {  // 没有别名
+                    sb.append(module.getName() + (module.isAsc() ? " ASC " : " DESC "));
+                }
+            } else {  // 有逗号
+                if (ParseUtil.isEmpty(module.getAlias())) {  // 有别名，别名优先
+                    sb.append(" , " + module.getAlias() + (module.isAsc() ? " ASC " : " DESC "));
+                } else {  // 没有别名
+                    sb.append(" , " + module.getName() + (module.isAsc() ? " ASC " : " DESC "));
+                }
+            }
+        }
+
+        return sb.toString();
     }
 
 }
