dd9392669bf7fc12e40d8de0d94a765d6f4ea7a5
ConanJordan <1042583309@qq.com>
2020-05-13 15:29:40 +0800

    update : ClientCommand.java, SelectOperate.java

--- src/main/java/pers/conan/easystorage/database/ClientCommand.java
+++ src/main/java/pers/conan/easystorage/database/ClientCommand.java
@@ -1,13 +1,77 @@
 package pers.conan.easystorage.database;
 
 import pers.conan.easystorage.annotation.Structure;
+import pers.conan.easystorage.operate.SelectOperate;
 
+import java.sql.Connection;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * 类：客户端命令
  */
 public class ClientCommand extends BaseCommand {
+
+    private Connection connection;
+    private String SQL;
+    private String condition;
+    private String sort;
+    private Object[] args;
+    private Structure target;
+    private Collection<Structure> targets;
+    private String seq;
+    private String table;
+    private SelectOperate select;
+
+    public String getSort() {
+        return sort;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public String getSQL() {
+        return SQL;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public Object[] getArgs() {
+        return args;
+    }
+
+    public Structure getTarget() {
+        return target;
+    }
+
+    public Collection<Structure> getTargets() {
+        return targets;
+    }
+
+    public String getSeq() {
+        return seq;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setSelect(SelectOperate select) {
+        this.select = select;
+    }
+
+    /**
+     * 构造方法
+     * @param connection
+     */
+    private ClientCommand(Connection connection) {
+        Objects.requireNonNull(connection); // 与数据库的连接必须真实有效
+        this.connection = connection;
+    }
+
     @Override
     public ClientCommand select(String table) {
         return null;
--- src/main/java/pers/conan/easystorage/operate/SelectOperate.java
+++ src/main/java/pers/conan/easystorage/operate/SelectOperate.java
@@ -1,10 +1,13 @@
 package pers.conan.easystorage.operate;
 
 import pers.conan.easystorage.database.ClientCommand;
+import pers.conan.easystorage.util.Sql;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
 
 /**
  * 类：查询操作
@@ -12,44 +15,67 @@ import java.sql.ResultSet;
 public class SelectOperate implements Operate {
 
     private ClientCommand command;
-    private Connection connection;
-    private PreparedStatement prst;
-    private ResultSet rs;
+    private Connection connection = null;
+    private PreparedStatement prst = null;
+    private ResultSet rs = null;
     private String SQL;
     private String table;
     private String condition;
     private String sort;
     private Object[] args;
 
-    private SelectOperate(
-            Connection connection,
-            String SQL, String table,
-            String condition,
-            String sort,
-            Object[] args,
-            ClientCommand command) {
-        this.connection = connection;
-        this.SQL = SQL;
-        this.table = table;
-        this.condition = condition;
-        this.sort = sort;
-        this.args = args;
-        this.command = command;
+    /**
+     * 构造方法
+     * 不对外开放
+     */
+    private SelectOperate(){
+
     }
 
-    public static SelectOperate build(
-            Connection connection,
-            String SQL,
-            String table,
-            String condition,
-            String sort,
-            Object[] args,
-            ClientCommand command) {
-        return new SelectOperate(connection, SQL, table, condition, sort, args, command);
+    /**
+     * 外部获取本类实例化对象的接口
+     */
+    public static SelectOperate build(ClientCommand command) {
+        SelectOperate instance = new SelectOperate(); // 获取本类的实例化对象
+
+        // 设置实例化对象的各个属性
+        instance.connection = command.getConnection();
+        instance.SQL = command.getSQL();
+        instance.condition = command.getCondition();
+        instance.args = command.getArgs();
+        instance.table = command.getTable();
+        instance.sort = command.getSort();
+
+        return instance; // 返回该实例化对象
     }
 
     @Override
     public ClientCommand operate() {
         return null; // TODO
+    }
+
+    private ClientCommand bySql() {
+        Objects.requireNonNull(this.SQL);  // SQL语句不能是空白
+
+        try {
+            // 准备要执行的SQL语句
+            this.prst = this.connection.prepareStatement(this.SQL);
+
+            // 设置参数
+            for (int i = 1; i <= this.args.length; i ++) {
+                this.prst.setObject(i, this.args[i]);
+            }
+
+            // 执行SQL语句
+            this.rs = this.prst.executeQuery();
+
+            // TODO 操作结果集
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // 释放数据库资源
+            Sql.close(new AutoCloseable[]{this.prst, this.rs});
+        }
     }
 }
