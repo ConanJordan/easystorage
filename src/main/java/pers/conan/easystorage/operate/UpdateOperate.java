6aa7ca288fb2d40d1b246b8c45d4026f5ada9614
ConanJordan <1042583309@qq.com>
2020-05-14 21:18:43 +0800

    add : 类：插入操作

---
+++ src/main/java/pers/conan/easystorage/operate/UpdateOperate.java
@@ -0,0 +1,20 @@
+package pers.conan.easystorage.operate;
+
+/**
+ * 类：删除操作
+ */
+public class UpdateOperate implements Operate {
+
+
+
+
+    @Override
+    public void prepare() throws Exception {
+
+    }
+
+    @Override
+    public void operate() {
+
+    }
+}
