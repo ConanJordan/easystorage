463cf6267b20d35fb73a09bf0f17ebbe34f1c30a
ConanJordan <1042583309@qq.com>
2020-05-15 09:46:07 +0800

    add : 类：删除操作

---
+++ src/main/java/pers/conan/easystorage/operate/DeleteOperate.java
@@ -0,0 +1,18 @@
+package pers.conan.easystorage.operate;
+
+/**
+ * 类：删除操作
+ *
+ * @author Conan
+ */
+public class DeleteOperate implements Operate {
+    @Override
+    public void prepare() throws Exception {
+
+    }
+
+    @Override
+    public void operate() {
+
+    }
+}
